a8313535045b15671b91fae0a843f125b9ab362a
Wendy Su <w696905@gmail.com>
2020-04-11 15:14:43 +0100

    without while True

--- password_setting.py
+++ password_setting.py
@@ -1,6 +1,6 @@
 password = 'a123456'
-chances = 3
-while True:
+chances = 3 #chances remain
+while chances != 0:
     password_setting = input('Please enter your password: ')
     if password_setting == password:
         print('Login Successfully!')
@@ -8,7 +8,7 @@ while True:
     else:
         chances = chances - 1
         print('Please try again.', chances, 'chance remain.')
-        if chances == 0:
-            print('No chance remain!')
-            break
+#if chances == 0:
+#print('No chance remain!')
+#break
         
